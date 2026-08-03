fa00bd9e0e92785da0add4746c25a6e95ecee997
starsli <starsli@tencent.com>
2026-08-03 15:02:50 +0800

    可观测

--- ordermgr.go
+++ ordermgr.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/starslipay/order_mgr/internal/config"
+	"github.com/starslipay/order_mgr/internal/metrics"
 	"github.com/starslipay/order_mgr/internal/server"
 	"github.com/starslipay/order_mgr/internal/svc"
 	"github.com/starslipay/order_mgr/order_mgr_pb"
@@ -32,6 +33,7 @@ func main() {
 			reflection.Register(grpcServer)
 		}
 	})
+	s.AddUnaryInterceptors(metrics.UnaryMetricInterceptor)
 	defer s.Stop()
 
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
